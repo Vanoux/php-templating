c300a9944996addfc0136c8877d4a0b3bc990f88
Vanoux <vanessa.roux891@orange.fr>
2018-08-04 00:36:50 +0200

    faker ok + mise en page

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once './vendor/autoload.php';
+require_once './vendor/autoload.php'; 
 
 $loader = new Twig_Loader_Filesystem('templates'); // pointe le chargement vers le dossier templates
 $twig = new Twig_Environment($loader, array( // déclare l'environnement twig
@@ -7,18 +7,26 @@ $twig = new Twig_Environment($loader, array( // déclare l'environnement twig
 ));
 $faker = Faker\Factory::create('fr_FR'); // déclare faker 
 
-// variable twig qui demande de charger l'index.html et rend un array avec des key = value faker
+// variable twig qui demande de charger l'index.html et rend un array avec des key = value(faker)
 echo $twig->render('index.html', array( 
     'company' => $faker->company,
     'catchPhrase' => $faker->catchPhrase,
-    'name' => $faker->name,
+    'adjective' => $faker->word,
+    'product' => $faker->text($maxNbChars = 15),
+    'material' => $faker->text($maxNbChars = 10),
     'url' => $faker->url,
+    'imgCard' => $faker->imageUrl($width = 200, $height = 200),
+    'color' => $faker->colorName,
+    'price' => $faker->randomDigitNotNull,
+    'imgCircle' => $faker->imageUrl($width, $height),
+    'name' => $faker->name,
     'jobTitle' => $faker->jobTitle,
+    'email' => $faker->email,
+    'phone' => $faker->phoneNumber,
+    'adress' => $faker->streetAddress,
+    'postCode' => $faker->postcode,
+    'city' => $faker->city,   
     ));
-
-
-
-
 
 ?>
 
